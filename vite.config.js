8fbdd06944a89e2cc7e31b785b54014ed1729924
NTabakharniuk <ntabaharnuk@gmail.com>
2026-02-03 14:23:29 +0200

    Revert "Configure GitHub Pages deployment"

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/bachelor_degree/',
   plugins: [react()],
   server: {
     port: 3000,
